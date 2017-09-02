c330092b1a52dc128f259142f3603b4aa357e1cc
babyChou <favorjump@gmail.com>
2017-09-02 23:36:52 +0800

    Update db.js

--- shared/db.js
+++ shared/db.js
@@ -1,16 +1,10 @@
 const AWS = require('aws-sdk');
 
-// AWS.config.update({
-//   region: "us-east-2",  
-//   accessKeyId: "AKIAJV3P5SQ5G553WAPQ",
-//   secretAccessKey: "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"  
-// });
-
 
 AWS.config.update({
-  region: "ap-southeast-1",  
-  accessKeyId: "AKIAJV3P5SQ5G553WAPQ",
-  secretAccessKey: "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"  
+  region: "",  
+  accessKeyId: "",
+  secretAccessKey: ""  
 });
 
 
